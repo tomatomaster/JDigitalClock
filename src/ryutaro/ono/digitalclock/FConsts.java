2fd13b3457fcf635250c6349a0a514648358fd89
tomatomaster <iiitomatomasteriii@gmail.com>
2016-03-01 22:31:31 +0900

    Add animation panel.

--- src/ryutaro/ono/digitalclock/FConsts.java
+++ src/ryutaro/ono/digitalclock/FConsts.java
@@ -3,8 +3,16 @@ package ryutaro.ono.digitalclock;
 public class FConsts {
   //FrameSize
   static final int WIDTH = 400;
-  static final int HEIGHT = 400;
+  static final int HEIGHT = 100;
   //Default axis on starting 
   static final int x = 400;
   static final int y = 400;
+  //FrameTitle
+  static final String TITLE = "DigitalClock";
+
+  /**
+   * AnimationPanel
+   */
+  static final int FRATE = 10; //ms
+  
 }
--- src/ryutaro/ono/digitalclock/JDigitalClockFrame.java
+++ src/ryutaro/ono/digitalclock/JDigitalClockFrame.java
@@ -1,17 +1,78 @@
 package ryutaro.ono.digitalclock;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
+import ryutaro.ono.digitalclock.service.clock.Clock;
 
+/**
+ * JDigitalClockFrame initialize Frame and animation Panel.
+ * @author ono
+ *
+ */
 public class JDigitalClockFrame extends JFrame {
 
-  
+  AnimationPanel animationP = new AnimationPanel();
   
   public JDigitalClockFrame() {
     initializeFrame();
+    addComponents();
+    setVisible(true);
   }
 
   private void initializeFrame() {
     setBounds(FConsts.x, FConsts.y, FConsts.WIDTH, FConsts.HEIGHT);
+    setTitle(FConsts.TITLE);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+  
+  private void addComponents() {
+    Container cp = getContentPane();
+    cp.add(animationP);
+  }
+  
+  /**
+   * 
+   * @author ono
+   *
+   */
+  class AnimationPanel extends JPanel implements ActionListener {
+    //Animation Timer
+    Timer timer;
+    int tCounter;
+    Clock clock;
+    
+    public AnimationPanel() {
+      clock = new Clock();
+      timer = new Timer(FConsts.FRATE, this); //Call ActionListener per FConsts.FRATE.
+      timer.setCoalesce(true);                //If system is busy and jam event messages then puts them into one.
+      timer.setRepeats(true);                 //If true then call actionPerformed repeatedly.
+      timer.start();
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+      paintClock(g);
+    }
+    
+    private void paintClock(Graphics g) {
+      g.setColor(Color.red);
+      g.drawString(clock.getTimeString(), 10, 10);
+    }
+
+    /**
+     * Timer call this method per {@code FConsts.FRATE}.
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      repaint();
+    }
   }
 }
